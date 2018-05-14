f1d68774c4d8f8c9b24cf8c0006711d1d361d857
Ryan John <34984049+PepperBurst@users.noreply.github.com>
2018-05-15 02:05:37 +0800

    Add files via upload

--- Flanger.py
+++ Flanger.py
@@ -2,11 +2,18 @@ import numpy as np
 from scipy.io import wavfile
 from scipy import signal
 import matplotlib.pyplot as plt
-plt.rcParams['agg.path.chunksize'] = 10000
+from decimal import Decimal
+# plt.rcParams['agg.path.chunksize'] = 10000
 # import math
+# M(o) = 13Hz
+# A = 12.8 ms
+# b = g + z-1
+# a = 1 + gz-1
+# g = 1-x / 1 + x
+
 
 def getWav(filename):
-    rate, data = wavfile.read(filename)
+    rate, data = wavfile.read('source_wav/'+filename)
     data = data/(32768.)
     data = np.float32(data)
     # plt.plot(range(0, len(data)), data)
@@ -15,28 +22,45 @@ def getWav(filename):
     # input()
     return rate, data
 
-def flang(filename):
-    rate, data = getWav(filename)
-
+def flang(filename, f_rate, depth, new_filename):
+    samp_rate, data = getWav(filename)
+    nsamp = len(data)
+    Mo = 0.013 * samp_rate
+    excursion = 1
     b = []
     b.append(1)
-    m_n1 = 2 * np.pi * 13/44100
-    m_n2 = m_n1 * float(len(data))
+    m_n1 = 2 * np.pi * f_rate/samp_rate
+    m_n2 = m_n1 * nsamp
     m_n3 = np.sin(m_n2)
-    m_n4 = 0.0128 * rate * m_n3
+    m_n4 = excursion * m_n3
     m_n5 = 1 + m_n4
-    m_n6 = int(m_n5)
-    for i in range(m_n6):
-        b.append(0)
-    b.append(0)
-    a = [1]
-    y = signal.lfilter(b, a, data)
-    y = np.float32(y)
-    # plt.plot(range(0, len(y)), y)
-    # plt.grid()
-    # plt.show()
-    # input()
-    wavfile.write(filename+'FLANG.wav', rate, y)
-    # y[n] = (1-0.25)x[n-100] + (0.25)x[n-101]
+    m_n6 = Mo * m_n5
+    if(not m_n6.is_integer()):
+        g_d = float(Decimal(str(m_n6)) % 1)
+        g_ = (1 - g_d) / (1 + g_d)
+        b_ = [g_, 1]
+        a_ = [1, g_]
+        M_ = m_n6 - g_d
+        b = []
+        b.append(1)
+        for i in range(int(M_)):
+            b.append(0)
+        b.append(depth)
+        a = [1]
+        data_1 = signal.lfilter(b, a, data)
+        data_2 = signal.lfilter(b_, a_, data_1)
+        final_data = data_2
+    else:
+        b.append(1)
+        for i in range(int(M_)):
+            b.append(0)
+        b.append(depth)
+        a = [1]
+        data_1 = signal.lfilter(b, a, data)
+        final_data = data_1
+    orig_filename = 'abcdc.com'
+    if filename.endswith('.wav'):
+        filename = filename[:-4]
+    wavfile.write(new_filename+'_FLANGED.wav', samp_rate, final_data)
 
-flang('guitar.wav')
+# flang('guitar.wav', 20, 1)
---
+++ Main.py
@@ -0,0 +1,43 @@
+import tkinter
+import Flanger as Fl
+from tkinter import Label
+from tkinter import Entry
+from tkinter import Scale
+from tkinter import Button
+from tkinter import messagebox
+def flangerCreate():
+    filename = entry_1.get()
+    rate = scale_1.get()
+    depth = scale_2.get()
+    new_filename = entry_2.get()
+    if(entry_1.get()==''):
+        messagebox.showinfo('Error', 'Empty filename!')
+    elif(entry_2.get()==''):
+        messagebox.showinfo('Error', 'Empty filename!')
+    else:
+        Fl.flang(filename+'.wav', rate, depth, new_filename)
+        messagebox.showinfo('Flanger', 'Flanged successfully!')
+    entry_1.delete(0, 'end')
+    entry_2.delete(0, 'end')
+    scale_1.set(1)
+    scale_2.set(0)
+
+top = tkinter.Tk()
+top.geometry('400x300')
+label_1 = Label(top, text='Digital Flanger Effect', relief='raised', justify='center', pady=4, width=50)
+label_2 = Label(top, text='Filename:', relief='groove', justify='left', pady=4, anchor='w', width=50)
+entry_1 = Entry(top, width=59)
+scale_1 = Scale(top, length = 360, from_=1, to=20, resolution=1, relief='sunken', label='Rate:', tickinterval=1, orient='horizontal', highlightcolor='green')
+scale_2 = Scale(top, length = 360, from_=0, to=1, resolution=0.01, relief='sunken', label='Depth:', orient='horizontal', highlightcolor='green')
+label_3 = Label(top, text='New filename:', relief='groove', justify='left', pady=4, anchor='w', width=50)
+entry_2 = Entry(top, width=59)
+button_1 = Button(top, text='Flange', justify='center', pady=4, width=50, relief='raised', command=flangerCreate)
+label_1.pack()
+label_2.pack()
+entry_1.pack()
+scale_1.pack()
+scale_2.pack()
+label_3.pack()
+entry_2.pack()
+button_1.pack()
+top.mainloop()
