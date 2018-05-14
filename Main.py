0533713afc89442375c33bf13ea16685dca21c09
Ryan John <34984049+PepperBurst@users.noreply.github.com>
2018-05-15 02:22:06 +0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -5,6 +5,9 @@ from tkinter import Entry
 from tkinter import Scale
 from tkinter import Button
 from tkinter import messagebox
+from tkinter import Frame
+from tkinter import Tk
+
 def flangerCreate():
     filename = entry_1.get()
     rate = scale_1.get()
@@ -22,24 +25,33 @@ def flangerCreate():
     scale_1.set(1)
     scale_2.set(0)
 
-top = tkinter.Tk()
-top.geometry('370x310')
-label_1 = Label(top, text='Digital Flanger Effect', relief='raised', justify='center', pady=4, width=50)
-label_2 = Label(top, text='Filename:', relief='groove', justify='left', pady=4, anchor='w', width=50)
-entry_1 = Entry(top, width=59)
-scale_1 = Scale(top, length = 360, from_=1, to=20, resolution=1, relief='sunken', label='Rate:', tickinterval=1, orient='horizontal', highlightcolor='green')
-scale_2 = Scale(top, length = 360, from_=0, to=1, resolution=0.01, relief='sunken', label='Depth:', orient='horizontal', highlightcolor='green')
-label_3 = Label(top, text='New filename:', relief='groove', justify='left', pady=4, anchor='w', width=50)
-entry_2 = Entry(top, width=59)
-button_1 = Button(top, text='Flange', justify='center', pady=4, width=50, relief='raised', command=flangerCreate)
-label_4 = Label(top, text='DSP Project by: Angelo Metran & Mart Jethro D.G. Javier', relief='raised', justify='center', pady=4, width=50)
-label_1.pack()
-label_2.pack()
-entry_1.pack()
-scale_1.pack()
-scale_2.pack()
-label_3.pack()
-entry_2.pack()
-button_1.pack()
-label_4.pack()
-top.mainloop()
+class Flanger(Frame):
+    def __init__(self,parent=None):
+        Frame.__init__(self,parent)
+        self.parent = parent
+        self.pack()
+        self.make_widgets()
+
+    def make_widgets(self):
+        self.winfo_toplevel().title("Digital Flanger Effect")
+        label_1 = Label(self, text='DSP Project by: Angelo Metran & Mart Jethro D.G. Javier', relief='raised', justify='center', pady=4, width=50)
+        label_2 = Label(self, text='Filename:', relief='groove', justify='left', pady=4, anchor='w', width=50)
+        entry_1 = Entry(self, width=59)
+        scale_1 = Scale(self, length = 360, from_=1, to=20, resolution=1, relief='sunken', label='Rate:', tickinterval=1, orient='horizontal', highlightcolor='green')
+        scale_2 = Scale(self, length = 360, from_=0, to=1, resolution=0.01, relief='sunken', label='Depth:', orient='horizontal', highlightcolor='green')
+        label_3 = Label(self, text='New filename:', relief='groove', justify='left', pady=4, anchor='w', width=50)
+        entry_2 = Entry(self, width=59)
+        button_1 = Button(self, text='Flange', justify='center', pady=4, width=50, relief='raised', command=flangerCreate)
+        label_1.pack()
+        label_2.pack()
+        entry_1.pack()
+        scale_1.pack()
+        scale_2.pack()
+        label_3.pack()
+        entry_2.pack()
+        button_1.pack()
+
+root = Tk()
+flanger = Flanger(root)
+root.geometry('370x290')
+root.mainloop()
